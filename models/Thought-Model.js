5f311c5ae8cc15d276287fb116d85b4423953592
Casres <christian@casillas.tech>
2022-07-28 11:58:41 -0400

    Added User Thought Model

---
+++ models/Thought-Model.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createddAt: {
+            type: Date,
+            default: Date.now(),
+            get: str => new Date(str).toLocaleString()
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const ThoughtSdchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: str => new Date(str).toLocaleString()
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thoughtr', ThoughtSchema);
+
+module.exports = Thought;

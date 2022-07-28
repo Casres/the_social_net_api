9ea054e4feb0e5528cd9f2380602ac76606d5c7a
Casres <christian@casillas.tech>
2022-07-28 11:59:17 -0400

    Refacored user thought model

--- models/Thought-Model.js
+++ models/Thought-Model.js
@@ -1,66 +1,66 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-const reactionSchema = new Schema (
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxLength: 280
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createddAt: {
-            type: Date,
-            default: Date.now(),
-            get: str => new Date(str).toLocaleString()
-        }
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true
-        },
-        id: false
-    }
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (str) => new Date(str).toLocaleString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-const ThoughtSdchema = new Schema (
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now(),
-            get: str => new Date(str).toLocaleString()
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (str) => new Date(str).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const Thought = model('Thoughtr', ThoughtSchema);
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;

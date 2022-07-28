85e0c3e67927e209b73bf22daedf603a78f042e0
Casres <christian@casillas.tech>
2022-07-28 12:06:01 -0400

    Refactored user-thoughts-routes file

--- routes/api/user-thoughts.js
+++ routes/api/user-thoughts.js
@@ -1,39 +1,32 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { 
-    getAllThoughts,
-    getThoughtById,
-    addThought, 
-    updateThought,
-    removeThought, 
-    addReaction, 
-    removeReaction } = require('../../controllers/user-thought-controller');
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/user-thought-controller");
 
 // set up GET all thoughts at /api/thoughts
-router
-    .route('/')
-    .get(getAllThoughts)
+router.route("/").get(getAllThoughts);
 
 // set up POST thoughts at /api/thoughts/:userId
-router
-    .route('/:userId')
-    .post(addThought);
+router.route("/:userId").post(addThought);
 
 // set up GET, PUT, and DELETE for thoughts at /api/thoughts/:userId/:thoughtId
 router
-    .route('/:userId/:thoughtId')
-    .get(getThoughtById)
-    .put(updateThought)
-    .delete(removeThought);
+  .route("/:userId/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
 
 // set up PUT reactions at /api/thoughts/:userId/:thoughtId/reactions
-router
-    .route('/:userId/:thoughtId/reactions')
-    .put(addReaction)
+router.route("/:userId/:thoughtId/reactions").put(addReaction);
 
 // set up DELETE reactions at /api/thoughts/:userId/:/thoughtId/:reactionId
-router
-    .route('/:thoughtId/reactions/:reactionId')
-    .delete(removeReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;

15b4798cef0fdb026f57e6bb1223104c55e15c25
Casres <christian@casillas.tech>
2022-07-28 12:06:29 -0400

    Refactored routes/api index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,13 @@
 // import all of the api routes to prefix their enpoint name and package them up
 
-const router = require('express').Router();
-const thoughtRoutes = require('./user-thoughts');
-const userRoutes = require('./user-routes');
+const router = require("express").Router();
+const thoughtRoutes = require("./user-thoughts");
+const userRoutes = require("./user-routes");
 
 // add prefix of /thoughts to routes created in thought-routes.js
-router.use('/thoughts', thoughtRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 // add prefix of /users to routes created in user-routes.js
-router.use('/users', userRoutes);
-
+router.use("/users", userRoutes);
 
 module.exports = router;
